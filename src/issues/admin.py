ca93f1490366fd21c19b5dcf5f41c9c42f5fc9b8
metamorfoso <ivan@numerical.co.nz>
2016-07-17 18:56:44 +1200

    Updated admin.

--- src/issues/admin.py
+++ src/issues/admin.py
@@ -3,7 +3,14 @@ from .models import Issue, Story
 from utilities.admin_shortcuts import inline_factory, single_page_admin
 
 
+ISSUE_FIELDS = [
+    'number',
+    'pub_date'
+]
+
+
 admin.site.register(
     Issue,
+    list_display=ISSUE_FIELDS,
     inlines=[inline_factory(Story)]
 )
