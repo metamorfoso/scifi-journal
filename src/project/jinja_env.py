4fe7a19e3fe493095d4519071f2c351efdddb1d6
metamorfoso <ivan@numerical.co.nz>
2016-07-18 22:39:51 +1200

    Fixed jinja env *properly*. Basic styling foundation.

--- src/project/jinja_env.py
+++ src/project/jinja_env.py
@@ -1,11 +1,9 @@
 from utilities import jinja_env
-from django.core import urlresolvers
 
 
 def environment(**options):
     env = jinja_env.environment(**options)
     env.globals.update({
         # project specific jinja env goes here
-        'url_for': urlresolvers.reverse
     })
     return env
