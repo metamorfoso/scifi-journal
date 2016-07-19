4a9859ab1b24956915358bba62ff095f8cd613fb
metamorfoso <ivan@numerical.co.nz>
2016-07-19 23:17:19 +1200

    Added author field to Story (fk to Author model).

--- src/issues/models.py
+++ src/issues/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    bio = models.TextField(max_length=250, blank=True)
+
+    def __str__(self):
+        return "%s, %s" % (self.last_name, self.first_name)
+
+
 class Issue(models.Model):
     pub_date = models.DateField(verbose_name='issue publication date')
     number = models.IntegerField(verbose_name='issue number')
@@ -20,17 +29,11 @@ class Issue(models.Model):
 
 class Story(models.Model):
     issue = models.ForeignKey(Issue)
+    author = models.ForeignKey(Author)
     title = models.CharField(max_length=100)
     content = models.FileField(upload_to='submission_uploads/')
 
     def __str__(self):
         return self.title
 
-
-class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    bio = models.TextField(max_length=250, blank=True)
-
-    def __str__(self):
-        return "%s, %s" % (self.last_name, self.first_name)
+# TODO: Review model
