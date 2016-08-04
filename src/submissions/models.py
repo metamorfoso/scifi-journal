63eb7138dc71c019337dcbd17c5d9a117056e246
metamorfoso <ivan@numerical.co.nz>
2016-08-04 23:44:40 +1200

    Added status field to Submission model.

--- src/submissions/models.py
+++ src/submissions/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+UNASSESSED = 0
+SHORTLISTED = 1
+REJECTED = 2
+ACCEPTED = 3
+
+SUBMISSION_STATES = (
+    (UNASSESSED, "Unassessed"),
+    (SHORTLISTED, "Shortlisted"),
+    (REJECTED, "Rejected"),
+    (ACCEPTED, "Accepted"),
+)
+
+
 class Submitter(models.Model):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=40, blank=True)
@@ -17,6 +30,8 @@ class Submission(models.Model):
     content = models.FileField(upload_to="submission_uploads/")
     author_notes = models.TextField(blank=True, max_length=3000)
     editor_notes = models.TextField(blank=True, max_length=3000)
+
+    status = models.IntegerField(default=0, choices=SUBMISSION_STATES)
 
     def __str__(self):
         return self.title
