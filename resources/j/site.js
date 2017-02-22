bec680a6712368f214a8b6ae4a7c6ce05384f74a
metamorfoso <ivan@numerical.co.nz>
2017-02-22 14:20:55 +1300

    displaying logo in mobile view, scrolling it at half speed

--- resources/j/site.js
+++ resources/j/site.js
@@ -2,17 +2,36 @@
 var lastScrollTop = 0;
 // Get the elements that will need their class updated
 var currentPanel = $('#current-panel');
-var logoPanel = $('#logo-panel');
+var logoPanel = $('#large-logo-panel');
+
+// Function for making the logo panel appear as if it's scrolling
+// at half speed when in mobile view
+var transformLogoPanel = function (destination) {
+  // logoPanel.css('transform', 'translateY(' + dest + 'px)');
+  logoPanel.css({
+    '-webkit-transform' : 'translateY(' + destination + 'px)',
+    '-moz-transform'    : 'translateY(' + destination + 'px)',
+    '-ms-transform'     : 'translateY(' + destination + 'px)',
+    '-o-transform'      : 'translateY(' + destination + 'px)',
+    'transform'         : 'translateY(' + destination + 'px)'
+  });
+}
 
 $(window).scroll(function () {
     // Compare current scroll top value to last known one to work out scrolling direction
     var scrollTop = $(this).scrollTop();
+    console.log(scrollTop)
+    var destination = scrollTop / 2  // destination for logo panel in mobile view
     if (scrollTop > lastScrollTop) {
         // downscroll
         currentPanel.removeClass('scrolling-up').addClass('scrolling-down');
+        logoPanel.removeClass('scrolling-up').addClass('scrolling-down');
+        transformLogoPanel(destination);
     } else {
         // upscroll
         currentPanel.removeClass('scrolling-down').addClass('scrolling-up');
+        logoPanel.removeClass('scrolling-down').addClass('scrolling-up');
+        transformLogoPanel(destination);
     }
     // When scrolltop hits 215, logo panel needs to be fixed in place
     if (scrollTop >= 215) {
